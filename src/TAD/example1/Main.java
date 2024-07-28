b663c44ddcbf953804c1e75d768349c281adea0b
Filipe Samuel <felipepires.p125@gmail.com>
2024-07-28 16:08:21 -0300

    EDIT: object type changed

--- src/TAD/example1/Main.java
+++ src/TAD/example1/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Point p1,p2;
-        ContractPoint pointTools = new Implementation();
+        Implementation pointTools = new Implementation();
 
         p1 = pointTools.createPoint(10,21);
         p2 = pointTools.createPoint(7,25);
