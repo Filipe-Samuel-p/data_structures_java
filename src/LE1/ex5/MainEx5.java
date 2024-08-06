0d31a4f4aa962d51586943c245cf050a9ef8d145
Filipe Samuel <felipepires.p125@gmail.com>
2024-08-06 00:48:10 -0300

    exercise 5 finished

--- src/LE1/ex5/MainEx5.java
+++ src/LE1/ex5/MainEx5.java
@@ -1,11 +1,10 @@
 package LE1.ex5;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainEx5 {
 
-    private static ArrayList<Integer> posicoes = new ArrayList<>();
+    private static final ArrayList<Integer> posicoes = new ArrayList<>();
 
     public static int totalSubstring(String[] string, String subString, int indexStart, int totalOcorrencias){
 
@@ -27,7 +26,7 @@ public class MainEx5 {
         String string = "Uma vez flamengo sempre flamengo flamengo flamengo";
         String[] vetorString = string.split(" ");
         String subString = "flamengo";
-        int totalOcorrencia = totalSubstring(vetorString,"flamengo",0,0);
+        int totalOcorrencia = totalSubstring(vetorString,subString,0,0);
         System.out.println("Ocorrencias: " + totalOcorrencia);
         System.out.println("Posições das substrings: " + posicoes);
     }
